a6d09d2beb7f35c1850ca65564358c1ca248b414
olavokruel <olavokruel@gmail.com>
2020-06-09 16:42:14 -0300

    added redux as a test

--- client/src/App.js
+++ client/src/App.js
@@ -1,27 +1,30 @@
-import React, { Component } from 'react';
+import React from 'react';
+import { useSelector } from 'react-redux';
 import './App.scss';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import OtherPage from './OtherPage';
 import Fib from './Fib';
 
-class App extends Component {
-  render() {
-    return (
-      <Router>
-        <div className="App">
-          <header className="App-header">
-            <h1 className="App-title">Fib Calculator KUBERNETES HTTPS UPDATED!!!</h1>
-            <Link to="/">Home</Link>
-            <Link to="/otherpage">Other Page</Link>
-          </header>
-          <div>
-            <Route exact path="/" component={Fib} />
-            <Route path="/otherpage" component={OtherPage} />
-          </div>
+const App = props => {
+
+  const test = useSelector(state => state.testReducer.test)
+
+  return (
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          This is the reducer working in production: {test}
+          <h1 className="App-title">Fib Calculator KUBERNETES HTTPS UPDATED!!!</h1>
+          <Link to="/">Home</Link>
+          <Link to="/otherpage">Other Page</Link>
+        </header>
+        <div>
+          <Route exact path="/" component={Fib} />
+          <Route path="/otherpage" component={OtherPage} />
         </div>
-      </Router>
-    );
-  }
+      </div>
+    </Router>
+  );
 }
 
 export default App;
